27cfbfa400432e782ac1ee06ed9e49baf11f2ca2
ewf-updater <ewf-updater@ewf.org>
2019-01-12 12:39:44 +0100

    added Homescreen

---
+++ components/HomeScreen.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Uhr from './Uhr.js'
+import Uhr2 from './Uhr2.js'
+import WeckerElement from './WeckerElement.js'
+
+export default class HomeScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      weckerArray: []
+    }
+
+    this.handleSwitchChange = this.handleSwitchChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.checkWecker = this.checkWecker.bind(this)
+  }
+
+  handleSwitchChange(weckerId) {
+    id = this.state.weckerArray.findIndex((element) => element.id == weckerId)
+    newWeckerArray = this.state.weckerArray
+    newWeckerArray[id].onOffSwitch = !newWeckerArray[id].onOffSwitch
+    this.setState({
+      weckerArray: newWeckerArray
+    })
+  }
+
+  handleClick(e) {
+    altesArray = this.state.weckerArray
+    altesArray.push({
+      id: altesArray.length,
+      weckerZeit: 1644884052,
+      whereAmI: "1.Nephi 4:5",
+      onOffSwitch: true
+    })
+    this.setState({
+      weckerArray: altesArray
+    })
+  }
+
+  checkWecker(currentTime) {
+    //check all wecker and compare timestamp
+    this.state.weckerArray.map((singleWecker) => {
+      singleWecker.weckerZeit < currentTime ? this.klingel() : null
+    })
+  }
+
+  klingel() {
+    console.log("kilingeling")
+    //setze Wecker neu
+
+  //rufe klingelfenster auf
+  //klingel im Fenster
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Uhr checkWecker={this.checkWecker}/>
+        <Uhr2></Uhr2>
+        {this.state.weckerArray ? this.state.weckerArray.map((singleWecker, index) => <WeckerElement weckerZeit={singleWecker.weckerZeit} onOffSwitch={singleWecker.onOffSwitch} handleSwitchChange={this.handleSwitchChange} id={singleWecker.id} key={index}/>) : null}
+        <Button title={"+"} onPress={this.handleClick} ></Button>
+      </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
